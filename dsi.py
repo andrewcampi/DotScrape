e9eb0b357f6d0c001c819782a8623438e27c4242
Andrew Campi <63204545+andrewcampi@users.noreply.github.com>
2023-12-28 10:18:07 -0500

    Update dsi.py

--- dsi.py
+++ dsi.py
@@ -7,16 +7,19 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, NoSuchFrameException, TimeoutException
-from selenium.common.exceptions import ElementNotInteractableException
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException, NoSuchFrameException, TimeoutException, ElementClickInterceptedException, ElementNotInteractableException
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+import undetected_chromedriver as uc
 
+
+BROWSER = "UndetectedChrome" # "Chrome", "UndetectedChrome", or "Firefox"
 
 def do_nothing():
     pass
@@ -82,20 +85,64 @@ def parse_scrape_content(dot_scrape_content):
     for line in lines[start_index:]:
         if line.strip():  # Only process lines with characters
             # Convert line to lowercase and strip escape characters, then add to commands list
-            commands.append(line.strip().lower().replace('\n', '').replace('\r', ''))
+            commands.append(line.strip().replace('\n', '').replace('\r', ''))
 
     return notes, commands
 
 
-def run_scraper(notes, commands, driver=None):
-    # Set up Chrome options
-    options = Options()
-    #options.add_argument("--headless")
-    #options.add_argument(f'user-agent={user_agent}')
+def click_on_text(driver, text_to_click):
+    found = False
+    for i in range(len(text_to_click), 1, -1):
+        substring_to_click = text_to_click[:i]
+        xpath_expression = f"//*[contains(text(), '{substring_to_click}')]"
+        try:
+            # Wait until the elements are available to be clicked or a timeout occurs
+            elements = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, xpath_expression))
+            )
+            if len(elements) == 1:
+                elements[0].click()  # Click on the element if only one is found
+                found = True
+                break
+            elif len(elements) > 1:
+                continue  # If more than one element is found, keep reducing the substring
+        except (NoSuchElementException, ElementClickInterceptedException, TimeoutException):
+            continue  # If an exception occurs, try with a shorter substring
 
-    # Initialize the Chrome driver using webdriver_manager
-    if driver == None:
-        driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
+    return found
+
+
+def run_scraper(notes, commands, driver=None, quiet=False, headless=False):
+    # Set up the driver
+    if BROWSER == "Firefox":
+        options = FirefoxOptions()
+        options.add_argument("--disable-web-security")
+        options.add_argument("--allow-running-insecure-content")
+        if headless:
+            options.add_argument("--headless")
+        #options.add_argument(f'user-agent={user_agent}')
+        # Initialize the driver using webdriver_manager
+        if driver == None:
+            driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+    elif BROWSER == "Chrome":
+        options = ChromeOptions()
+        options.add_argument("--disable-web-security")
+        options.add_argument("--allow-running-insecure-content")
+        if headless:
+            options.add_argument("--headless")
+        #options.add_argument(f'user-agent={user_agent}')
+        # Initialize the driver using webdriver_manager
+        if driver == None:
+            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    elif BROWSER == "UndetectedChrome":
+        options = uc.ChromeOptions()
+        options.add_argument("--no-sandbox")
+        if headless:
+            driver = uc.Chrome(options=options, headless=True, version_main=119)
+        else:
+            driver = uc.Chrome(options=options, version_main=119)
+    else:
+        exit()
 
     auto_solve_recaptcha = False
     for note in notes:
@@ -106,22 +153,25 @@ def run_scraper(notes, commands, driver=None):
 
     # Process each command
     for command in commands:
-        time_to_wait = round(random.uniform(1, 3), 5)
-        print(f"WAITING: {time_to_wait} seconds")
-        sleep(time_to_wait)
+        case_sensitive_command = command
+        command = command.lower()
+        if not ("store the current url" in command or "snapshot this page into a variable" in command or command.startswith("return") or command.startswith("wait")):
+            time_to_wait = round(random.uniform(1, 3), 5)
+            print(f"WAITING: {time_to_wait} seconds") if not quiet else None
+            sleep(time_to_wait)
 
         if auto_solve_recaptcha:
             try:
-                print("--> Looking for recaptcha to solve")
+                print("--> Looking for recaptcha to solve") if not quiet else None
                 result = solveRecaptcha(driver)
-                print("--> SOLVED")
+                print("--> SOLVED") if not quiet else None
                 driver.switch_to.default_content()
                 sleep(round(random.uniform(1, 2), 5))
             except:
-                print("--> Could not find a recatpcha")
+                print("--> Could not find a recatpcha") if not quiet else None
                 do_nothing()
 
-        print("DOING:", command)
+        print("DOING:", command) if not quiet else None
 
         if command.startswith('go to '):
             # Extract the URL and navigate to it
@@ -131,17 +181,23 @@ def run_scraper(notes, commands, driver=None):
             driver.get(url)
 
         elif command.startswith('go back'):
-            # TODO: Hit the back button to go back one page
             driver.back()
 
+        elif command.startswith('click on the element with class name "') and "or the text" in command:
+            parts = command.split('"')
+            class_to_click = parts[1].strip()
+            alternative_text = parts[3].strip().lower()  # The second substring inside double quotes
+            try:
+                element = driver.find_element(By.CLASS_NAME, class_to_click)
+                # Click on the element
+                element.click()
+            except:
+                click_on_text(alternative_text)
+
         elif command.startswith('click on the text "'):
+            print("Command:", command)
             text_to_click = command.split('"')[1]
-            # Create an XPath expression that matches any element containing the specified text
-            xpath_expression = f"//*[contains(text(), '{text_to_click}')]"
-            # Find the element using the XPath selector
-            element = driver.find_element(By.XPATH, xpath_expression)
-            # Click on the found element
-            element.click()
+            click_on_text(driver, text_to_click)
 
         elif command.startswith('click on the div with class'):
             class_to_click = command.split('"')[1]  # Extract the class name from the command
@@ -149,11 +205,43 @@ def run_scraper(notes, commands, driver=None):
             xpath_expression = f"//div[contains(@class, '{class_to_click}')]"
             # Find the element using the XPath selector
             elements = driver.find_elements(By.XPATH, xpath_expression)
+            element_found_and_clicked = False
             # Click the first visible div element with the specified class
             for element in elements:
                 if element.is_displayed():  # Check if the element is visible
                     element.click()  # Click on the visible element
+                    element_found_and_clicked = True
                     break
+
+            # If no visible element was clicked, search within iframes and popups
+            if not element_found_and_clicked:
+                # Switch to each iframe and search for the element
+                iframes = driver.find_elements(By.TAG_NAME, "iframe")
+                for iframe in iframes:
+                    driver.switch_to.frame(iframe)
+                    elements_in_iframe = driver.find_elements(By.XPATH, xpath_expression)
+                    for element in elements_in_iframe:
+                        if element.is_displayed():
+                            element.click()
+                            element_found_and_clicked = True
+                            break
+                    driver.switch_to.default_content()  # Switch back to the main content
+                    if element_found_and_clicked:
+                        break
+
+                # If element is still not found, search in any popups
+                if not element_found_and_clicked:
+                    for handle in driver.window_handles:
+                        driver.switch_to.window(handle)
+                        elements_in_popup = driver.find_elements(By.XPATH, xpath_expression)
+                        for element in elements_in_popup:
+                            if element.is_displayed():
+                                element.click()
+                                element_found_and_clicked = True
+                                break
+                        if element_found_and_clicked:
+                            break
+
 
         elif command.startswith('click on the div with id'):
             id_to_click = command.split('"')[1]  # Extract the id from the command
@@ -167,19 +255,34 @@ def run_scraper(notes, commands, driver=None):
                     element.click()  # Click on the visible element
                     break
 
+        elif command.startswith('slow type "'):
+            # Extract the text
+            text_to_type = case_sensitive_command.split('"')[1]
+            # Initialize ActionChains
+            actions = webdriver.ActionChains(driver)
+            # Iterate over each character in the string
+            for char in text_to_type:
+                # Type the character
+                actions.send_keys(char).perform()    
+                # Wait for a random time between 0.02 and 0.5 seconds
+                sleep(random.uniform(0.02, 0.5))
+
         elif command.startswith('type "'):
             # Extract the text and type it into the focused element
-            text_to_type = command.split('"')[1]
+            text_to_type = case_sensitive_command.split('"')[1]
             webdriver.ActionChains(driver).send_keys(text_to_type).perform()
 
         elif ('enter' in command) and (command.startswith('click') or command.startswith('press') or command.startswith('hit') or command.startswith('type')):
             # Simulate pressing Enter
             webdriver.ActionChains(driver).send_keys(Keys.ENTER).perform()
 
-        elif command == 'wait forever':
+        elif command.startswith('wait forever'):
             # Wait indefinitely
             while True:
                 sleep(1)
+
+        elif command.startswith("wait for user"):
+            input("===> Waiting for user. Press [ENTER] to proceed when ready.")
 
         elif command.startswith('wait '):
             # Extract the number of seconds and wait
@@ -275,13 +378,13 @@ def run_scraper(notes, commands, driver=None):
 
         elif command.startswith('solve') and "recaptcha" in command:
             try:
-                print("--> Looking for recaptcha to solve")
-                result = solveRecaptcha(driver)
-                print("--> SOLVED")
+                print("--> Looking for recaptcha to solve") if not quiet else None
+                result = solveRecaptcha(driver) 
+                print("--> SOLVED") if not quiet else None
                 driver.switch_to.default_content()
                 sleep(round(random.uniform(1, 2), 5))
             except:
-                print("--> Could not find a recatpcha")
+                print("--> Could not find a recatpcha") if not quiet else None
                 do_nothing()
 
         elif command.startswith('snapshot this page into a variable'):
@@ -301,7 +404,7 @@ def run_scraper(notes, commands, driver=None):
                 try:
                     element = driver.find_element(By.CLASS_NAME, item_name)
                 except selenium.common.exceptions.NoSuchElementException:
-                    print("\n======= AN ERROR OCCURED =======")
+                    print("\n======= AN ERROR OCCURED =======") if not quiet else None
                     raise Exception(f'Could not find the class "{item_name}".')
                 visible_text_content = element.text
                 variables[variable_name] = visible_text_content.strip().lower()
@@ -310,7 +413,7 @@ def run_scraper(notes, commands, driver=None):
                 try:
                     element = driver.find_element(By.ID, item_name)
                 except selenium.common.exceptions.NoSuchElementException:
-                    print("\n======= AN ERROR OCCURED =======")
+                    print("\n======= AN ERROR OCCURED =======") if not quiet else None
                     raise Exception(f'Could not find the id "{item_name}".')
                 visible_text_content = element.text
                 variables[variable_name] = visible_text_content.strip().lower()
@@ -338,9 +441,9 @@ def run_scraper(notes, commands, driver=None):
     return None
 
 # IF IMPORTING FROM ANOTHER FILE (i.e. you did "import dsi"), simply provide your multi-line .scrape language as an argument and run this function.
-def run_dot_scrape(dot_scrape_content, driver=None):
+def run_dot_scrape(dot_scrape_content, driver=None, quiet=False, headless=False):
     notes, commands = parse_scrape_content(dot_scrape_content)
-    return run_scraper(notes, commands, driver=driver) # Returns a dictionary with (at most) keys "driver" and "variables"
+    return run_scraper(notes, commands, driver=driver, quiet=quiet, headless=headless) # Returns a dictionary with (at most) keys "driver" and "variables"
 
 
 # MAIN -- You can use this code to run the DSI from the command line, given a .scrape file path as an argument.
